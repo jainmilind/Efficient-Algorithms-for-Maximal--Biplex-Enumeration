cdb8ac87d5e030bed22a125d81a62234a6d3b063
Mokshith <mokshith002@gmail.com>
2023-03-01 23:04:26 +0530

    Added check for k-biplex and partition of R for enumAlmostSat

--- Graph.cpp
+++ Graph.cpp
@@ -1,36 +1,90 @@
 #include "Graph.h"
+#include <vector>
 
 using namespace std;
 
-// class Graph{
-
-//     public:
-//     int size;
-//     int left_vertices;
-//     int k;
-//     vector<vector<int>> adj;
-//     vector<bool> solution;
-//     set<int> R_keep, R1, R2;
-
-//     Graph(vector<vector<int>>&, int, int);
-//     bool is_kbiplex();
-//     void initial_soultion();
-// };
 
 Graph::Graph(vector<vector<int>> &adj, int left_vertices, int k){
     this -> adj = adj;
     this -> left_vertices = left_vertices;
     this -> k = k;
     size = adj.size();
-    R_keep.clear();
-    R1.clear();
-    R2.clear();
-    solution.resize(size, false);
+    solution.resize(size, 0);
 }
 
+bool Graph::is_kbiplex(vector<int> &solution){
 
+    for(int i = 0; i < size; i++){
+        if(!solution[i])
+            continue;
+        
+        int disconnections = 0;
 
-void Graph::initial_soultion(){
+        for(auto v : adj[i]){
+            solution[v] *= -1;
+        }
+        
+        // i is a vertex in the left part
+        if(i < left_vertices){
+            for(int j = left_vertices; j < size; j++)
+                disconnections += solution[j] == 1;
+        }
+
+        // i is a vertex in the right part
+        else{
+            for(int j = 0; j < left_vertices; j++)
+                disconnections += solution[j] == 1;
+        }   
+
+        for(auto v : adj[i]){
+            solution[v] *= -1;
+        }   
+
+        if(disconnections > k)
+            return false;
+    }
+
+    return true;
+}
+
+vector<int> Graph::initial_soultion(){
+
+    vector<int> solution(size, 0);
+
     fill(solution.begin() + left_vertices, solution.end(), 1);
+    
+    for(int i = 0; i < left_vertices; i++){
+        solution[i] = 1;
+        if(!is_kbiplex(solution)){
+            solution[i] = 0;
+        }
+    }
+
+    return solution;
+
+}
+
+void Graph::partition_R(vector<int> &L, vector<int> &R, int v, vector<int> &R_keep, vector<int> &R1, vector<int> &R2, vector<vector<int>> &H_adj){
+    
+    for(auto u : H_adj[v]){
+        R_keep.push_back(u);
+    }
+
+    for(auto u : R){
+        if(find(R_keep.begin(), R_keep.end(), u) != R.end())
+            continue;
+        if(H_adj[u].size() == k + 1)
+            R2.push_back(u);
+        else
+            R1.push_back(u);
+    }
+
+
+}
+
+vector<vector<int>> Graph::enumAlmostSat(vector<int> &L, vector<int> &R, int v,  vector<vector<int>> &H_adj){
+    
+    vector<int> R_keep, R1, R2;
+    partition_R(L, R, v, R_keep, R1, R2, H_adj);
 
 }
--- Graph.h
+++ Graph.h
@@ -9,11 +9,12 @@ class Graph{
     int left_vertices;
     int k;
     vector<vector<int>> adj;
-    vector<bool> solution;
-    set<int> R_keep, R1, R2;
+    vector<int> solution;
 
     Graph(vector<vector<int>>&, int, int);
-    bool is_kbiplex();
-    void initial_soultion();
+    bool is_kbiplex(vector<int>&);
+    vector<int> initial_soultion();
+    vector<vector<int>> enumAlmostSat(vector<int>&, vector<int>&, int, vector<vector<int>>&);
+    void partition_R(vector<int>&, vector<int>&, int, vector<int>&, vector<int>&, vector<int>&, vector<vector<int>>&);    
 };
 
