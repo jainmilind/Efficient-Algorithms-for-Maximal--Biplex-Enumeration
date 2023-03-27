ac8b57b862bba6b73ad9210e3d487cdd5e39f396
jainmilind <jainmilind10112002@gmail.com>
2023-03-27 23:41:46 +0530

    Made minor changes

--- Graph.cpp
+++ Graph.cpp
@@ -1,9 +1,13 @@
 #include "Graph.h"
-#include <algorithm>
-#include <iterator>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& p) { return os << "(" << p.first << ", " << p.second << ")"; }
+template <typename T_container, typename T = typename enable_if < !is_same<T_container, string>::value, typename T_container::value_type >::type > ostream& operator<<(ostream& os, const T_container& v) { os << '{'; string sep; for (const T& x : v) os << sep << x, sep = ", "; return os << '}'; }
+template <typename T> void debug_out(string s, T t) { cout << "[" << s << ": " << t << "]\n"; }
+template <typename T, typename... U> void debug_out(string s, T t, U... u) { int w = 0, c = 0; while (w < (int)s.size()) { if (s[w] == '(') c++; if (s[w] == ')') c--; if (!c and s[w] == ',') break; w++; } cout << "[" << s.substr(0, w) << ": " << t << "] "; debug_out(s.substr(w + 2, (int)s.size() - w - 1), u...); }
+#define dbg(x...) debug_out(#x, x)
+
 
 
 Graph::Graph(vector<set<int>>& adj, int left_vertices, int k) {
@@ -15,7 +19,7 @@ Graph::Graph(vector<set<int>>& adj, int left_vertices, int k) {
 }
 
 // boolean array of taken
-bool Graph::is_kbiplex(vector<int>& solution) {
+bool Graph::is_kbiplex(vector<bool>& solution) {
 
     int cnt_left_solution = 0;
     for (int i = 0; i < left_vertices; ++i)
@@ -29,8 +33,9 @@ bool Graph::is_kbiplex(vector<int>& solution) {
             cnt_left_taken += solution[x];
 
         int disconnections = cnt_left_solution - cnt_left_taken;
-        if (disconnections > k)
+        if (disconnections > k) {
             return false;
+        }
     }
 
     int cnt_right_solution = 0;
@@ -45,8 +50,9 @@ bool Graph::is_kbiplex(vector<int>& solution) {
             cnt_right_taken += solution[x];
 
         int disconnections = cnt_right_solution - cnt_right_taken;
-        if (disconnections > k)
+        if (disconnections > k) {
             return false;
+        }
     }
 
 
@@ -67,63 +73,6 @@ vector<bool> Graph::initial_soultion() {
     }
 
     return solution;
-}
-
-void Graph::partition_R(vector<int>& L, vector<int>& R, int v, vector<int>& R_keep, vector<int>& R1, vector<int>& R2) {
-
-    for (auto u : R) {
-        if (adj[v].count(u))
-            R_keep.push_back(u);
-        else {
-            int cnt = 0;
-            for (auto v : L) {
-                if (!adj[u].count(v))
-                    cnt++;
-            }
-            if (cnt < k)
-                R1.push_back(v);
-            else
-                R2.push_back(v);
-        }
-    }
-}
-
-vector<int> now;
-void generate_all_subset(int i, int cnt, int limit, vector<int>& R, vector<vector<int>>& all_subset) {
-    if (cnt > limit) return;
-    if (i == R.size()) {
-        all_subset.push_back(now);
-        return;
-    }
-
-    // Swapped recursive calls so that the subsets returned would be sorted by size.
-    generate_all_subset(i + 1, cnt, limit, R, all_subset);
-    now.push_back(R[i]);
-    generate_all_subset(i + 1, cnt + 1, limit, R, all_subset);
-    now.pop_back();
-}
-
-
-void enumerate_r_enum_subset(vector<int>& R1, vector<int>& R2, int k, vector<vector<int>>& all_subset_r1, vector<vector<int>>& all_subset_r2, int* pos) {
-    int total_size = R1.size() + R2.size();
-    // R1 U R2 all subsets
-    now.clear();
-    generate_all_subset(0, 0, k, R1, all_subset_r1);
-
-    now.clear();
-    generate_all_subset(0, 0, k, R2, all_subset_r2);
-
-    /* Commented because they will be sorted by size anyway **/
-    // sort(all_subset_r1.begin(), all_subset_r1.end(), [](auto& a, auto& b) {return a.size() < b.size(); });
-    // sort(all_subset_r2.begin(), all_subset_r2.end(), [](auto& a, auto& b) {return a.size() < b.size(); });
-
-    //pos[i] gives the index of the first susbet of r2 with size i.
-    pos[0] = 0;
-    pos[k + 1] = all_subset_r2.size();
-    for (int i = 1; i < all_subset_r2.size(); i++) {
-        if (all_subset_r2[i] != all_subset_r2[i - 1])
-            pos[all_subset_r2[i].size()] = i;
-    }
 }
 
 // Not O(|L|*|R|)
@@ -162,6 +111,67 @@ bool Graph::is_local_solution(vector<int>& L, vector<int>& R, vector<int>& L_sub
     return true;
 }
 
+
+void Graph::partition_R(vector<int>& L, vector<int>& R, int v, vector<int>& R_keep, vector<int>& R1, vector<int>& R2) {
+
+    for (auto u : R) {
+        if (adj[v].count(u))
+            R_keep.push_back(u);
+        else {
+            int cnt = 0;
+            for (auto v : L) {
+                if (!adj[u].count(v))
+                    cnt++;
+            }
+            if (cnt < k)
+                R1.push_back(u);
+            else
+                R2.push_back(u);
+        }
+    }
+}
+
+vector<int> now;
+void generate_all_subset(int i, int cnt, int limit, vector<int>& R, vector<vector<int>>& all_subset) {
+    if (cnt > limit) return;
+    if (i == R.size()) {
+        all_subset.push_back(now);
+        return;
+    }
+
+    // Swapped recursive calls so that the subsets returned would be sorted by size.
+    generate_all_subset(i + 1, cnt, limit, R, all_subset);
+    now.push_back(R[i]);
+    generate_all_subset(i + 1, cnt + 1, limit, R, all_subset);
+    now.pop_back();
+}
+
+
+void enumerate_r_enum_subset(vector<int>& R1, vector<int>& R2, int k, vector<vector<int>>& all_subset_r1, vector<vector<int>>& all_subset_r2, vector<int>& pos) {
+    int total_size = R1.size() + R2.size();
+    // R1 U R2 all subsets
+    now.clear();
+    generate_all_subset(0, 0, k, R1, all_subset_r1);
+
+    now.clear();
+    generate_all_subset(0, 0, k, R2, all_subset_r2);
+
+    /* Commented because they will be sorted by size anyway **/
+    sort(all_subset_r1.begin(), all_subset_r1.end(), [](auto& a, auto& b) {return a.size() < b.size(); });
+    sort(all_subset_r2.begin(), all_subset_r2.end(), [](auto& a, auto& b) {return a.size() < b.size(); });
+    dbg(R1, all_subset_r1);
+    dbg(R2, all_subset_r2);
+
+    //pos[i] gives the index of the first subset of r2 with size i.
+    pos[0] = 0;
+    pos[k + 1] = all_subset_r2.size();
+    for (int i = 1; i < all_subset_r2.size(); i++) {
+        if (all_subset_r2[i] != all_subset_r2[i - 1])
+            pos[all_subset_r2[i].size()] = i;
+    }
+}
+
+
 /**
 L -> Left vertices in the previous solution
 R -> Right vertices in the previous solution
@@ -178,14 +188,27 @@ vector<vector<int>> Graph::enumAlmostSat(vector<int>& L, vector<int>& R, int v) 
     partition_R(L, R, v, R_keep, R1, R2);
 
     vector<vector<int>> r1_powset, r2_powset;
-    int pos[k + 2];
+    vector<int> pos(k + 2, -1);
     enumerate_r_enum_subset(R1, R2, k, r1_powset, r2_powset, pos);
 
+    dbg(pos);
+    for (int i = 0; i <= k + 1;) {
+        int j = i + 1;
+        while (j <= k + 1 and pos[j] == -1) {
+            pos[j] = pos[i] + 1;
+            j++;
+        }
+        i = j;
+    }
+    dbg(pos);
+    for (int i = 0; i < r2_powset.size(); ++i)
+        dbg(i, r2_powset[i]);
+    cout << endl;
     vector<vector<int>> local_solutions;
 
     for (auto& r1_subset : r1_powset) {
-
         int size1 = r1_subset.size();
+        // size2 is R2 subset size limit
         int size2 = k - size1;
 
         int start = 0;
@@ -196,9 +219,11 @@ vector<vector<int>> Graph::enumAlmostSat(vector<int>& L, vector<int>& R, int v) 
             start = pos[size2];
             end = pos[size2 + 1];
         }
+        dbg(r1_subset, size1, size2);
+        dbg(start, end);
 
-        for (int i = start; i < end; i++) {
-
+        for (int i = start; i < end; ++i) {
+            assert(r2_powset.size() > i);
             vector<int>& r2_subset = r2_powset[i];
             vector<int> r_enum_subset, r_subset;
 
@@ -256,7 +281,7 @@ vector<bool> Graph::extendToMax(vector<int>& L_loc, vector<int>& R_loc) {
     iota(L.begin(), L.end(), 0);
 
     vector<int> L_diff;
-    set_difference(L.begin(), L.end(), L_loc.begin(), L_loc.end(), back_inserter(L_diff));
+    set_difference(L.begin(), L.end(), L_loc.begin(), L_loc.end(), begin(L_diff));
 
     vector<bool> taken(size);
     for (int i : L_loc) taken[i] = true;
@@ -297,6 +322,7 @@ void Graph::ThreeStep(vector<bool>& h0, set<vector<bool>>& ans) {
             vector<bool> H_max = extendToMax(L_loc, R_loc);
 
             if (!ans.count(H_max)) {
+                cout << H_max << endl;
                 ans.insert(H_max);
                 ThreeStep(H_max, ans);
             }
@@ -309,5 +335,8 @@ set<vector<bool>> Graph::btraversal() {
 
     set<vector<bool>> ans;
     ans.insert(h0);
+    cout << "h0 " << h0 << endl;
+    ThreeStep(h0, ans);
 
+    return ans;
 }
--- Graph.h
+++ Graph.h
@@ -2,9 +2,9 @@
 
 using namespace std;
 
-class Graph{
+class Graph {
 
-    public:
+public:
     int size; // number of vertices
     int left_vertices; // number of vertices in the left subset of vertices
     int k; // maximum number of disconnections
@@ -13,10 +13,13 @@ class Graph{
     vector<bool> solution; // a maximal k-biplex, true -> vertex is in solution else not present
 
     Graph(vector<set<int>>&, int, int);
-    bool is_kbiplex(vector<int>&);
-    vector<int> initial_soultion();
+    bool is_kbiplex(vector<bool>&);
     vector<vector<int>> enumAlmostSat(vector<int>&, vector<int>&, int);
-    void partition_R(vector<int>&, vector<int>&, int, vector<int>&, vector<int>&, vector<int>&);    
+    void partition_R(vector<int>&, vector<int>&, int, vector<int>&, vector<int>&, vector<int>&);
     bool is_local_solution(vector<int>&, vector<int>&, vector<int>&, vector<int>&, int);
+    vector<bool> initial_soultion();
+    vector<bool> extendToMax(vector<int>&, vector<int>&);
+    void ThreeStep(vector<bool>&, set<vector<bool>>&);
+    set<vector<bool>> btraversal();
 };
 
